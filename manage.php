b3ae5c537937a27673c348ea2640353cbe4ef4ee
Stha-Parichit <119142592+Stha-Parichit@users.noreply.github.com>
2024-04-09 20:32:44 +0545

    Updated a little

--- manage.php
+++ manage.php
@@ -40,9 +40,74 @@ $conn->close();
     <title>Manage Reservations</title>
     <style>
         /* Css hala */
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+        }
+        #sidebar {
+            position: fixed;
+            left: 0;
+            top: 0;
+            width: 200px;
+            height: 100%;
+            background-color: #614f4f;
+            padding-top: 100px;
+            color: #fff;
+        }
+        #sidebar ul {
+            list-style-type: none;
+            padding: 0;
+            margin: 0;
+        }
+        #sidebar ul li {
+            padding: 10px;
+            border-bottom: 1px solid #000000;
+        }
+        #sidebar ul li a{
+            color: black;
+            size: 40px;
+            font-size: larger;
+            
+        }
+        #sidebar ul li:hover {
+            background-color: #c8b6b6;
+        }
+        #content {
+            margin-left: 220px;
+            padding: 20px;
+        }
+        h1 {
+            color: #000000;
+        }
+        .button-container {
+            text-align: center;
+            margin-top: 50px;
+        }
+        .button {
+            padding: 10px 20px;
+            background-color: #4CAF50;
+            color: white;
+            border: none;
+            border-radius: 5px;
+            font-size: 16px;
+            cursor: pointer;
+        }
+        .button:hover {
+            background-color: #45a049;
+        }
     </style>
 </head>
 <body>
+    <div id="sidebar">
+        <ul>
+            <li><a href="#">Admins</a></li>
+            <li><a href="manage.php">Manage Reservations</a></li>
+            <li><a href="#">View Reservations</a></li>
+            <li><a href="#">Edit Reservation</a></li>
+            <li><a href="#">User Feedbacks</a></li>
+        </ul>
+    </div>
     <div id="content">
         <h1>Manage Reservations</h1>
         
@@ -73,6 +138,11 @@ $conn->close();
         <?php else : ?>
             <p>No reservations found.</p>
         <?php endif; ?>
+        
+        <!-- Button to return back to the dashboard -->
+        <div class="button-container">
+            <a href="dashboard.html" class="button">Back to Dashboard</a>
+        </div>
     </div>
 </body>
 </html>
